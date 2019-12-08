8e82ac974378b6d14d182fee661cfc7f56570eed
Doni Setiawan <donisetiawan.050@gmail.com>
2019-12-08 18:52:26 +0700

    End-to-end testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
@@ -12,10 +12,35 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testHomePageRedirection()
     {
-        $response = $this->get('/cats');
+        $response = $this->get('/');
+        $response->assertRedirect('cats');
+    }
 
+    public function testVisitorIsRedirected()
+    {
+        $response = $this->get('/cats/create');
+        $response->assertRedirect('login');
+    }
+
+    public function testLoggedInUserCanCreateCat()
+    {
+        $user = new User(['name'     => 'John Doe',
+                          'is_admin' => false]);
+        $this->be($user);
+        $response = $this->get('/cats/create');
         $response->assertStatus(200);
+    }
+
+    public function testNonAdminCannotEditCat()
+    {
+        $user = new User(['id'       => 2,
+                          'name'     => 'User #2',
+                          'is_admin' => false]);
+        $this->be($user);
+        $response = $this->delete('/cats/2');
+        $response->assertRedirect('/cats/2');
+        $response->assertSessionHas('error');
     }
 }
